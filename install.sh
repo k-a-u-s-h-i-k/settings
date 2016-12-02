e0ad5caa9066b7af3f426fb009431081142c7b84
kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2016-12-02 15:30:13 -0500

    Install zsh if not installed

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 pwd=`pwd`
 git_prog=`command -v git`
+zsh_prog=`command -v zsh`
 
 if [ -z "${git_prog}"]; then
 	echo "Git not installed on the system. Installing now..." 
@@ -9,15 +10,20 @@ if [ -z "${git_prog}"]; then
 fi
 
 #clone my repo
-echo =============== Cloning Kaushiks repo ================="
+echo "=============== Cloning Kaushik's repo ================="
 git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
 cd ~/.settings
 
 #switch to the coop branch
 git checkout coop
 
+#install zsh if not installed
+if [ -z "${zsh_prog}" ]; then
+	sudo apt-get install zsh -y
+fi
+
 #Install Oh my Zsh
-echo =============== Installing OH MY ZSH ================="
+echo "=============== Installing OH MY ZSH ================="
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #setup softlinks
