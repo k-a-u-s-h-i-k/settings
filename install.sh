f42a20e3ed0286ec0f40b0e3b9567430d6a53e6f
kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2016-12-02 15:26:00 -0500

    Add print statements

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ if [ -z "${git_prog}"]; then
 fi
 
 #clone my repo
+echo =============== Cloning Kaushiks repo ================="
 git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
 cd ~/.settings
 
@@ -16,6 +17,7 @@ cd ~/.settings
 git checkout coop
 
 #Install Oh my Zsh
+echo =============== Installing OH MY ZSH ================="
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #setup softlinks
