d8024e7c2f7d2dae54d4623d9dde1cb053ee7bdf
kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2016-12-02 15:48:45 -0500

    More robust install script

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ pwd=`pwd`
 git_prog=`command -v git`
 zsh_prog=`command -v zsh`
 
-if [ -z "${git_prog}"]; then
+if [ -z "${git_prog}" ]; then
+	echo ""
 	echo "Git not installed on the system. Installing now..." 
 	sudo apt-get install git -y
 fi
 
 #clone my repo
+echo ""
 echo "=============== Cloning Kaushik's repo ================="
 git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
 cd ~/.settings
@@ -19,15 +21,18 @@ git checkout coop
 
 #install zsh if not installed
 if [ -z "${zsh_prog}" ]; then
+	echo ""
 	echo "=============== Installing ZSH ================="
 	sudo apt-get install zsh -y
 fi
 
 #Install Oh my Zsh
+echo ""
 echo "=============== Installing OH MY ZSH ================="
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -
 
 #setup softlinks
+echo ""
 echo "=============== Setup Soft Links ================="
 ln -s ~/.settings/.vimrc ~/.vimrc 
 mv ~/.zshrc ~/.zshrc.orig.ohmyzsh
