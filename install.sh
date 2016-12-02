20a43fa6c4105c8eb29121265f8f8a4988dd918f
kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2016-12-02 15:11:40 -0500

    More robust install script

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 pwd=`pwd`
 git_prog=`command -v git`
 
-if [ -n "${git_prog}" ]; then
-	#clone my repo
-	git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
-	cd ~/.settings
-
-	#switch to the coop branch
-	git checkout coop
-else
+if [ -z "${git_prog}"]; then
 	echo "Git not installed on the system. Installing now..." 
 	sudo apt-get install git -y
 fi
+
+#clone my repo
+git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
+cd ~/.settings
+
+#switch to the coop branch
+git checkout coop
 
 #Install Oh my Zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
