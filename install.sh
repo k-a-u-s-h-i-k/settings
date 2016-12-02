a0921316810d57834ac427e4b30205cd25f62413
kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2016-12-02 15:02:24 -0500

    Add git install option to install script

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+pwd=`pwd`
+git_prog=`command -v git`
+
+if [ -n "${git_prog}" ]; then
+	#clone my repo
+	git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
+	cd ~/.settings
+
+	#switch to the coop branch
+	git checkout coop
+else
+	echo "Git not installed on the system. Installing now..." 
+	sudo apt-get install git -y
+fi
+
 #Install Oh my Zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-pwd=`pwd`
-
-git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
-cd ~/.settings
-git checkout coop
-
+#setup softlinks
 ln -s ~/.settings/.vimrc ~/.vimrc 
 ln -s ~/.settings/.zshrc ~/.zshrc 
