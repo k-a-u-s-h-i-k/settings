58fc58ebd799f5dcd4fd54f7ca866ff2b4adc92a
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2017-08-22 22:05:53 -0400

    turn of trackstick on system start

--- dock.sh
+++ dock.sh
@@ -8,8 +8,7 @@ if [ $num_monitors -gt 1 ]; then
 	xrandr --output DP2 --primary
 	i3-msg workspace 2
 	i3-msg workspace number 1
-#	i3-msg workspace number 1 output DP2 
-#	i3-msg workspace number 2 output DP2
-#	i3-msg workspace number 3 output DP2
-#	i3-msg workspace number 4 output DP2
+else
+	#turn off track stick
+	xinput -set-prop "AlpsPS/2 ALPS DualPoint Stick" "Device Enabled" 0
 fi
