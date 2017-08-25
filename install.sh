36ba34a18c8f0a8d1a625764208f0b4251e5d4c9
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2017-08-24 20:49:55 -0400

    Introduce colours in install script

--- install.sh
+++ install.sh
@@ -4,15 +4,41 @@ pwd=`pwd`
 git_prog=`command -v git`
 zsh_prog=`command -v zsh`
 
-if [ -z "${git_prog}" ]; then
+# Use colors, but only if connected to a terminal, and that terminal supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
+
+output()
+{
 	echo ""
-	echo "Git not installed on the system. Installing now..." 
+	printf "${GREEN}"
+	echo $1
+	printf "${NORMAL}"
+}
+
+if [ -z "${git_prog}" ]; then
+    output "============== Installing Git =========================="
 	sudo apt-get install git -y
 fi
 
 #clone my repo
-echo ""
-echo "=============== Cloning Kaushik's repo ================="
+output "=============== Cloning Kaushik's repo ================="
 git clone https://github.com/k-a-u-s-h-i-k/settings.git ~/.settings
 cd ~/.settings
 
@@ -21,14 +47,12 @@ git checkout coop
 
 #install zsh if not installed
 if [ -z "${zsh_prog}" ]; then
-	echo ""
-	echo "=============== Installing ZSH ================="
+	output "=============== Installing ZSH ================="
 	sudo apt-get install zsh -y
 fi
 
 #Install Oh my Zsh
-echo ""
-echo "=============== Installing OH MY ZSH ================="
+output "=============== Installing OH MY ZSH ================="
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O oh.sh
 sed -i '/env zsh/d' oh.sh
 sed -i '/chsh -s/d' oh.sh
@@ -37,17 +61,16 @@ chmod +x oh.sh
 rm -f ./oh.sh
 
 #setup softlinks
-echo ""
-echo "=============== Setup Soft Links ================="
-ln -s ~/.settings/.vimrc ~/.vimrc 
-echo "\"Add your custom vim settings to this file" > ~/.myvimrc
-echo "#Add your custom zsh settings to this file" > ~/.myzshrc
+output "=============== Setup Soft Links ================="
+ln -s ~/.settings/.vimrc ~/.vimrc
+output "\"Add your custom vim settings to this file" > ~/.myvimrc
+output "#Add your custom zsh settings to this file" > ~/.myzshrc
 mv ~/.zshrc ~/.zshrc.orig.ohmyzsh
-ln -s ~/.settings/.zshrc ~/.zshrc 
-echo "=============== Soft links setup done ================="
+ln -s ~/.settings/.zshrc ~/.zshrc
+output "=============== Soft links setup done ================="
 
 
-echo "=============== Setup cscope maps ====================="
+output "=============== Setup cscope maps ====================="
 if [ ! -d ~/.vim ]; then
 	mkdir ~/.vim #if ~/.vim dir doesn't exist, create it
 fi
@@ -58,7 +81,7 @@ cd ~/.vim/plugin
 wget http://cscope.sourceforge.net/cscope_maps.vim
 cd - # go back to the previous directory
 
-echo "=============== Download badwolf theme ====================="
+output "=============== Download badwolf theme ====================="
 if [ ! -d ${HOME}/.vim/colors ]; then
     mkdir ${HOME}/.vim/colors
 fi
@@ -66,25 +89,26 @@ cd ${HOME}/.vim/colors
 wget https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim
 cd -
 
-echo "=============== Setup Makefile ftplugin ==============="
+output "=============== Setup Makefile ftplugin ==============="
 if [ ! -d ~/.vim/ftplugin ]; then
 	mkdir ~/.vim/ftplugin
 fi
 cd ~/.vim/ftplugin
+#do not expand tabs in a makefile
 echo "set noexpandtab" > make.vim
 cd -
 
-echo "=============== Install vim-gtk to get global clipboard support ==============="
+output "=============== Install vim-gtk to get global clipboard support ==============="
 sudo apt install vim-gtk
 
-echo "=============== Setup Go directories ==============="
+output "=============== Setup Go directories ==============="
 if [ ! -d ~/.go-dirs ]; then
     mkdir ~/.go-dirs
 fi
 
-echo "=============== vimrc and zshrc files are now in your home folder ================="
-echo "=============== Add custom vim settings to .myvimrc and zsh settings to .myzshrc files ============"
-echo "=============== Setup successful =================="
+output "=============== vimrc and zshrc files are now in your home folder ================="
+output "=============== Add custom vim settings to .myvimrc and zsh settings to .myzshrc files ============"
+output "=============== Setup successful =================="
 
 #Check this site for how to setup cron task for auto git pull
 #https://thoughtsimproved.wordpress.com/2015/08/17/self-updating-git-repos/
