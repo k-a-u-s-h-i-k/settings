e7267eb2b06ec63ad3b97df4b327123254fd8587
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2018-01-23 21:51:21 -0500

    Reinstate gnome-terminal setup

--- install.sh
+++ install.sh
@@ -150,6 +150,25 @@ cd fonts
 #install all fonts
 ./install.sh
 
+output "=============== Setting up gnome-terminal ==============="
+# uncheck use system font in gnome-terminal
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type=boolean false
+# set gnome-terminal to use powerline font
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Meslo LG S DZ for Powerline Regular 12"
+
+# find Terminal UUID to settings can be edited
+UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
+
+# enable custom command
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ use-custom-command true
+
+# specify zsh as the custom command to run
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ custom-command /usr/bin/zsh
+
+# uncheck use system theme colour gnome-terminal setting
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ use-theme-colors false
+
+
 output "=============== Setting up Terminator ==============="
 ~/.settings/terminator/terminator.sh
 
