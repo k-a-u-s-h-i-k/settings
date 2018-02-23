bcc906af538db607e563b1da6f4f8a530434757f
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2018-02-22 20:16:32 -0500

    Update i3 install script

--- i3/i3.sh
+++ i3/i3.sh
@@ -4,8 +4,8 @@
 set -e
 
 mkdir /tmp/i3 && cd /tmp/i3
-/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
-sudo apt install ./keyring.deb
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
+sudo dpkg -i ./keyring.deb
 source=$(eval "echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe"")
 echo $source | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
 
