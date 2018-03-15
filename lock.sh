72544be4b4d251faa558e1b65f8ae70df62dd84b
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2018-03-15 17:27:31 -0400

    Randamize lock screen wallpaper

--- lock.sh
+++ lock.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 num_monitors=$(eval "xrandr --listmonitors | grep Monitors | cut -d":" -f 2")
-echo $num_monitors
 
 if [ $num_monitors -gt 1 ]; then
 	xrandr --output eDP1 --off
 fi
 
-i3lock -i ~/Downloads/lock.png
+wallpaper_path=${HOME}/Pictures/wallpapers/png
+image=$(ls $wallpaper_path | sort -R | tail -1)
+i3lock -i "$wallpaper_path/$image"
