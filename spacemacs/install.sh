baeb9bf256c9bbdb24e5baaaccaf551670d75972
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2018-05-17 07:11:05 -0400

    spacemacs install script

---
+++ spacemacs/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ln -s ~/.settings/spacemacs/spacemacs ~/.spacemacs
+
+# install this for org mode tex/pdf exports
+# This is going to download > 1GB of files...
+sudo apt install texlive-latex-base
+sudo apt install texlive-latex-recommended
+sudo apt install texlive-fonts-recommended
+sudo apt install texlive-latex-extra
+
+#needed for ssh
+sudo apt install ssh-askpass
+
+#needed to export org docs to pdfs
+sudo apt install texlive-full
