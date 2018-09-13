eeb0b8f6dc5de85cccbe94417cb550f771f88cf9
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2018-09-13 12:44:27 -0400

    Install libs reuired for pdf tools

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -14,3 +14,6 @@ sudo apt install ssh-askpass
 
 #needed to export org docs to pdfs
 sudo apt install texlive-full
+
+#needed to use pdf-tools layer
+sudo apt install libpng-dev zlib1g-dev libpoppler-glib-dev libpoppler-private-dev
