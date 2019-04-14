c8aac0f302f148ba66dc6ffd479ff077266744ef
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2019-04-14 17:29:09 -0400

    Initial commit for spacevim config

---
+++ spacevim/install.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install neovim
+sudo apt install python3-neovim
+sudo apt install python-neovim
+
+wget https://spacevim.org/install.sh | bash
+
+ln -s ~/.settings/spacevim ~/.SpaceVim.d
