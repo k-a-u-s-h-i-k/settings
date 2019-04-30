64c2e3b860c174773866778e3cce95f4c7418655
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2019-04-30 11:24:37 -0400

    Update polybar install for Ubuntu 18.04

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -2,12 +2,13 @@
 
 cd /tmp
 
-git clone --branch 3.0.5 --recursive https://github.com/jaagr/polybar
+rm -rf polybar
+
+git clone --branch 3.3 --recursive https://github.com/jaagr/polybar
 mkdir polybar/build
 cd polybar/build
 
-#sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto i3-wm libasound2-dev libmpdclient-dev libiw-dev
-sudo apt install libcairo2-dev cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev
+sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
 
 cmake ..
 sudo make install
