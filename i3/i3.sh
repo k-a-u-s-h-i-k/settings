3d38beadd8bf0f96d34eeb234629c958991a73e2
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2019-05-23 14:23:36 -0400

    Fix i3 install script

--- i3/i3.sh
+++ i3/i3.sh
@@ -5,8 +5,9 @@ set -e
 
 mkdir /tmp/i3 && cd /tmp/i3
 /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
-dpkg -i ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+sudo dpkg -i ./keyring.deb
+source=$(eval "echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe"")
+echo $source | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
 
 sudo apt update
 
