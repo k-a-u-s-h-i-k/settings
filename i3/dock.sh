274c5127547a8e767f6417e9203e0b3c22802e05
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2021-04-01 09:07:36 -0400

    Start polybar on every connected monitor

--- i3/dock.sh
+++ i3/dock.sh
@@ -10,3 +10,15 @@ else
 	#turn off track stick
 	xinput -set-prop "AlpsPS/2 ALPS DualPoint Stick" "Device Enabled" 0
 fi
+
+pkill polybar
+
+#launch polybar for each connected monitor
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
