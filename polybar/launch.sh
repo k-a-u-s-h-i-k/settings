36eb90c39f6c0935a356707ff08b54049cba648d
Kaushik <k-a-u-s-h-i-k@users.noreply.github.com>
2021-07-20 20:32:13 -0400

    Use polybar launch.sh script

---
+++ polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pkill polybar
+
+#launch polybar for each connected monitor
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+
